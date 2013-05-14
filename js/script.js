c120782d2c5b86a877382ba15e979d5fa8743395
Eugene Sia <eugene@redkeydigital.com>
2013-05-14 14:17:02 +0100

    Add basic styles and JS

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Script file.
+ * Copyright 2013, Eugene Sia
+ * s i a e u g e n e [at] g m a i l [dot] c o m
+ */
+
+/**
+ * Plan: 
+ * Build a selector library.
+ * Research into animation.
+ * Build an animation library.
+ * Use selectors to animate.
+ */
+
+// Anonymous closure, prevents collisions.
+(function(window, undefined) {
+
+window.onload = function() {
+	
+};
+
+}(window);
